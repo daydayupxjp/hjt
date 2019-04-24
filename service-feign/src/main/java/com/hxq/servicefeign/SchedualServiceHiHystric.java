1ccd2100554790a80ad5c863258fc105c3fa1461
hxq245338 <hero457422093@sina.com>
2019-04-24 19:43:58 +0800

    springcloud first blood

---
+++ service-feign/src/main/java/com/hxq/servicefeign/SchedualServiceHiHystric.java
@@ -0,0 +1,11 @@
+package com.hxq.servicefeign;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements  SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return name + "error";
+    }
+}
